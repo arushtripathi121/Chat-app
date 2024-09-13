df7cf43b8f84ae63f43bb5471a69bda82d772784
arushtripathi121 <arushtripathi121@gamail.com>
2024-09-13 16:35:00 +0530

    c

--- client/src/components/ContactComponent.jsx
+++ client/src/components/ContactComponent.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ContactComponentImage from '../assets/chatBoxPhoto.jpeg'
 const ContactComponent = () => {
+
     return (
-        <div className='px-1 py-1 bg-gray-200'>
+        <div className='px-1 py-1'>
             <button className='w-full flex flex-row items-center gap-5 '>
                 <img src={ContactComponentImage} className='w-[60px] rounded-[60px]' />
                 <p className='flex flex-row justify-between items-center w-full pr-5'> 
