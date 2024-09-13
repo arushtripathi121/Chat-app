d98ad87c2e93adcce786baa4fbe5f43fc9a00549
arushtripathi121 <arushtripathi121@gamail.com>
2024-09-13 16:32:47 +0530

    c

---
+++ client/src/components/ContactComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ContactComponentImage from '../assets/chatBoxPhoto.jpeg'
+const ContactComponent = () => {
+    return (
+        <div className='px-1 py-1 bg-gray-200'>
+            <button className='w-full flex flex-row items-center gap-5 '>
+                <img src={ContactComponentImage} className='w-[60px] rounded-[60px]' />
+                <p className='flex flex-row justify-between items-center w-full pr-5'> 
+                    <span className='text-xl font-medium'>Aditya Giri</span>
+                    <span className='text-sm'>17:51</span>
+                </p>
+            </button>
+        </div>
+    )
+}
+
+export default ContactComponent
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ContactComponent from '../components/ContactComponent';
 
 const HomePage = () => {
 
@@ -24,12 +25,49 @@ const HomePage = () => {
       navigate('/login')
     }
   }, [])
-  
+
   return (
-    <div>
-      This is home page
-      <button className='bg-red-600 text-white text-xl font-semibold px-2 py-2' onClick={handleLogout}>Logout</button>
-    </div>
+    <main className='flex w-screen h-screen'>
+      <nav className='w-[15svh] border border-black'>
+
+      </nav>
+
+      <section className=' w-[100svh] flex flex-col'>
+        <div className='h-[20svh]'> 
+          <p className='text-2xl font-semibold px-5 pt-5'>Chats</p>
+          <p className='px-2 pt-4'><input type='text' className='w-full outline-none px-3 py-2 text-lg font-medium rounded-2xl bg-gray-100 ' placeholder='Search or start a new chat' /></p>
+        </div>
+        <div className='flex flex-col pt-2 component-with-scrollbar overflow-y-auto h-[110svh]'>
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+          <ContactComponent />
+        </div>
+      </section>
+
+      <section className='bg-homeChat w-[290svh] h-screen border border-black rounded-lg'>
+      </section>
+
+    </main>
   )
 }
 
